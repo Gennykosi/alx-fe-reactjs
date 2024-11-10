9878605ebdfc972473001728fc4beea30e57095b
Georgian <kosigeorgian@gmail.com>
2024-11-10 19:23:33 +0900

    enhance changes

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 
 function  MainContent(){
     return (
-    <main style={{ backgroundColor: 'skyblue', color: 'white', textAlign: 'center' }}>
+    <main>
         <p>I love to visit New York, Paris, and Tokyo.</p>
     </main> 
     );
