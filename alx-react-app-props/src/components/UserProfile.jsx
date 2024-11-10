c9ee92fed16a29da2b223b0b69cf339dd2ef5ea4
Georgian <kosigeorgian@gmail.com>
2024-11-11 06:28:43 +0900

    commit features

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,17 @@
- 
-  function UserProfile (props){
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
 
-  export default UserProfile; 
+import React, { useContext } from 'react'; // Import React and useContext
+import UserContext from './UserContext';   // Import UserContext
+
+function UserProfile() {
+  // Consume userData from UserContext
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h1>{userData.name}</h1>
+      <p>{userData.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
