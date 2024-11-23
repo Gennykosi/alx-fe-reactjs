32db2f56df7e112e55e5992f785c4f6e76b34f3b
Georgian <kosigeorgian@gmail.com>
2024-11-24 07:11:04 +0900

    commit features

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchPosts = async () => {
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return data;
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading, refetch } = useQuery('posts', fetchPosts, {
+    staleTime: 5000, // Cache data for 5 seconds
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading posts.</div>;
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <button onClick={refetch}>Refetch Posts</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
