f1e6f6d4ff087d89c45236345da617ceeb3d9893
Georgian <kosigeorgian@gmail.com>
2024-11-24 18:09:35 +0900

    changes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import Home from './components/Home';
+import Profile from './components/Profile';
 import ProfileDetails from './components/ProfileDetails';
 import ProfileSettings from './components/ProfileSettings';
 import UserProfile from './components/UserProfile';
+import BlogPost from './components/BlogPost';
 
 const ProtectedRoute = ({ isAuthenticated, children }) => {
   return isAuthenticated ? children : <Navigate to="/" />;
@@ -19,6 +21,7 @@ const App = () => {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/profile">Profile</Link></li>
           <li><Link to="/user/123">User Profile</Link></li>
+          <li><Link to="/blog/1">Blog Post</Link></li>
         </ul>
       </nav>
 
@@ -31,15 +34,16 @@ const App = () => {
               <Profile />
             </ProtectedRoute>
           }
-        />
+        >
+          {/* Nested Routes */}
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
         <Route path="/user/:userId" element={<UserProfile />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
-      <Route path="details" element={<ProfileDetails />} />
-      <Route path="settings" element={<ProfileSettings />} />
-
     </Router>
   );
 };
 
 export default App;
-
