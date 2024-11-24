2728bd780c8801e8456d121b8b04ccc56156b0a8
Georgian <kosigeorgian@gmail.com>
2024-11-24 17:52:30 +0900

    commit

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import Home from './components/Home';
-import Profile from './components/Profile';
 import ProfileDetails from './components/ProfileDetails';
 import ProfileSettings from './components/ProfileSettings';
 import UserProfile from './components/UserProfile';
@@ -35,6 +34,9 @@ const App = () => {
         />
         <Route path="/user/:userId" element={<UserProfile />} />
       </Routes>
+      <Route path="details" element={<ProfileDetails />} />
+      <Route path="settings" element={<ProfileSettings />} />
+
     </Router>
   );
 };
